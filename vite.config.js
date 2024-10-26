1d588067f025288f745d9ca5daa4aa6bf4cdb5ed
Caitlyn Caldwell <cecldwll@gmail.com>
2024-10-26 13:32:40 -0600

    show genre.html

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        genre: resolve(__dirname, "src/genre.html")
       }
     }
   }
