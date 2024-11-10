ed2dd96f5fa6e6fcb6f572ef240c2b5ebb252758
Caitlyn Caldwell <cecldwll@gmail.com>
2024-11-09 22:47:36 -0700

    my last hope pls work

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: "src/",
+  root: "",
 
   build: {
     outDir: "../dist",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/index.html"),
-        genre: resolve(__dirname, "src/genre.html")
+        main: resolve(__dirname, "index.html"),
+        genre: resolve(__dirname, "genre.html")
       }
     }
   }
